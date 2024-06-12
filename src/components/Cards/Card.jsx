b0e3d90351fe0d94fbf1786df7b708b44db973ab
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-12 15:50:23 +0200

    add > map method for card item

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,21 +1,53 @@
-import cardStyle from './Card.module.css'
+import cardStyle from "./Card.module.css";
 
 function Card() {
+  const card = [
+    {
+      title: "Titolo1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      title: "Titolo2",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      title: "Titolo3",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      title: "Titolo4",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      title: "Titolo5",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      title: "Titolo6",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+  ];
+
   return (
     <>
-      <div className={cardStyle.card}>
-        <div className={cardStyle.cardImage}>
-          <img src="https://picsum.photos/200/200" alt="" />
+      {card.map((item) => (
+        <div className={cardStyle.card}>
+          <div className={cardStyle.cardImage}>
+            <img src={item.image} alt="" />
+          </div>
+          <div className={cardStyle.cardContent}>
+            <h2 className="cardTitle">{item.title}</h2>
+            <p className="cardText">{item.text}</p>
+            <button className={cardStyle.cardBtn}>LEGGI DI PIÙ</button>
+          </div>
         </div>
-        <div className={cardStyle.cardContent}>
-            <h2 className="cardTitle">Titolo</h2>
-          <p className="cardText">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa,
-            quae.
-          </p>
-          <button className={cardStyle.cardBtn}>LEGGI DI PIÙ</button>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
