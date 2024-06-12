5631e037d8ed05cf01ae1d852f6493d7ae3e089a
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-12 17:04:44 +0200

    add > passing props from main to card

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,48 +1,10 @@
 import cardStyle from "./Card.module.css";
 
-function Card() {
-  const cards = [
-    {
-      id: 1,
-      title: "Titolo1",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-    {
-      id: 2,
-      title: "Titolo2",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-    {
-      id: 3,
-      title: "Titolo3",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-    {
-      id: 4,
-      title: "Titolo4",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-    {
-      id: 5,
-      title: "Titolo5",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-    {
-      id: 6,
-      title: "Titolo6",
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
-      image: "https://picsum.photos/200/200",
-    },
-  ];
+function Card({cardProps}) {
 
   return (
     <>
-      {cards.map((card) => (
+      {cardProps.map((card) => (
         <div className={cardStyle.card} key={card.id}>
           <div className={cardStyle.cardImage}>
             <img src={card.image} alt="" />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,13 +3,50 @@ import mainStyle from './Main.module.css'
 
 
 function Main() {
-
+  const cards = [
+    {
+      id: 1,
+      title: "Titolo1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 2,
+      title: "Titolo2",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 3,
+      title: "Titolo3",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 4,
+      title: "Titolo4",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 5,
+      title: "Titolo5",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 6,
+      title: "Titolo6",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
+      image: "https://picsum.photos/200/200",
+    },
+  ];
     return (
       <>
         <main className={mainStyle.pageMain}>
           <div className="container">
             <div className={mainStyle.cards}>
-              <Card/>
+              <Card cardProps={cards}/>
             </div>
           </div>
         </main>
