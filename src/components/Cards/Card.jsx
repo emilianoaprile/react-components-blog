5a0639d5ea5c176d3f5a57c7201de20f0d58bef3
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-12 16:06:06 +0200

    add > key id into map method

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,33 +1,39 @@
 import cardStyle from "./Card.module.css";
 
 function Card() {
-  const card = [
+  const cards = [
     {
+      id: 1,
       title: "Titolo1",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
     },
     {
+      id: 2,
       title: "Titolo2",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
     },
     {
+      id: 3,
       title: "Titolo3",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
     },
     {
+      id: 4,
       title: "Titolo4",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
     },
     {
+      id: 5,
       title: "Titolo5",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
     },
     {
+      id: 6,
       title: "Titolo6",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quae.",
       image: "https://picsum.photos/200/200",
@@ -36,14 +42,14 @@ function Card() {
 
   return (
     <>
-      {card.map((item) => (
-        <div className={cardStyle.card}>
+      {cards.map((card) => (
+        <div className={cardStyle.card} key={card.id}>
           <div className={cardStyle.cardImage}>
-            <img src={item.image} alt="" />
+            <img src={card.image} alt="" />
           </div>
           <div className={cardStyle.cardContent}>
-            <h2 className="cardTitle">{item.title}</h2>
-            <p className="cardText">{item.text}</p>
+            <h2 className="cardTitle">{card.title}</h2>
+            <p className="cardText">{card.text}</p>
             <button className={cardStyle.cardBtn}>LEGGI DI PIÙ</button>
           </div>
         </div>
